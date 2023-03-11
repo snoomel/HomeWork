e2e3d9264ba75add50ab36123e29a7f230b7195a
snoomel <20katerinka01@gmail.com>
2023-03-11 22:23:45 +0700

    initial project Add hw for lesson 1

---
+++ Lesson1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Lesson1;
+public class Program
+{ 
+    public static void Main(string[] args)
+    {
+        string MyName;
+        Console.WriteLine("Введите свое имя");
+        MyName = Console.ReadLine();
+        if (MyName != "")
+            Console.WriteLine("Hello, " + MyName);
+        else
+            Console.WriteLine("Hello, друг");
+    }
+    
+}
